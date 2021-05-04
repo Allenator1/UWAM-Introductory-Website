22df607e8ee6327bbf8fb1fc4206ddd9f9d78b59
Allen Antony <22706998@student.uwa.edu.au>
2021-05-05 01:59:47 +0800

    Added basic flask functionality - routes to '/' and '/login', basic form handling for login

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,15 @@
+from flask import render_template, redirect, url_for
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
-@app.route('/index')
 def index():
-    return "Hello World"
+    user = {'username': 'Allenator', 'preferred_name': 'Allen'}
+    return render_template('index.html', user=user)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        return redirect('/index')
+    return render_template(url_for('login_or_register'), type="login", form=form)
