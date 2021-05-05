4596a4fd8ca3b4d29ed9cd7ca8cb0f43723677ec
Allen Antony <22706998@student.uwa.edu.au>
2021-05-06 02:06:00 +0800

    Added route to /register, routing to the login_or_register.html file

--- app/routes.py
+++ app/routes.py
@@ -1,15 +1,23 @@
 from flask import render_template, redirect, url_for
 from app import app
-from app.forms import LoginForm
+from app.forms import LoginForm, RegisterForm
 
 @app.route('/')
 def index():
-    user = {'username': None, 'preferred_name': None}
+    user = {'username': 'Allen', 'preferred_name': 'A'}
     return render_template('index.html', user=user)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        return redirect('/')
-    return render_template('login_or_register.html', type="login", form=form)
+        return redirect(url_for('index'))
+    return render_template('login_or_register.html', type="Login", form=form)
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        return redirect(url_for('index'))
+    return render_template('login_or_register.html', type="Register", form=form)
+
