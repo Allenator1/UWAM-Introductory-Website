ae4569911dfe258da038bbb12e7e02902af3e017
Allen Antony <22706998@student.uwa.edu.au>
2021-05-06 23:19:38 +0800

    Created function for '/logout'. Changed login function to query the database (for validation) and use flask_login to set current_user

--- app/routes.py
+++ app/routes.py
@@ -1,23 +1,42 @@
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, flash
+from flask_login import current_user, login_user, logout_user
 from app import app
 from app.forms import LoginForm, RegisterForm
+from app.models import User
+
 
 @app.route('/')
 def index():
-    user = {'username': 'Allen', 'preferred_name': 'A'}
-    return render_template('index.html', user=user)
+    return render_template('index.html')
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        else:
+            login_user(user, remember=form.remember_me.data)
+            return redirect(url_for('index'))
+    return render_template('login&register.html', type='Login', form=form)
+
+
+@app.route('/logout')
+def logout():
+    if current_user.is_authenticated:
+        logout_user()
         return redirect(url_for('index'))
-    return render_template('login_or_register.html', type="Login", form=form)
+
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegisterForm()
     if form.validate_on_submit():
         return redirect(url_for('index'))
-    return render_template('login_or_register.html', type="Register", form=form)
+    return render_template('login&register.html', type="Register", form=form)
 
