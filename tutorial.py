a4cfdccd63cf66afdac362d27a561b1f9663ea58
Allen Antony <22706998@student.uwa.edu.au>
2021-05-06 18:34:30 +0800

    Added 'make_shell_context' function to automatically import items in the flask shell

--- tutorial.py
+++ tutorial.py
@@ -1,4 +1,9 @@
-from app import app
+from app import app, db
+from app.models import User, Quiz, Tutorial
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Quiz': Quiz, 'Tutorial': Tutorial}
 
 if __name__=="__main__":
-    app.run(host='127.0.0.9', port=4455, debug=True)
+    app.run(host='127.0.0.9', port=5000, debug=True)
