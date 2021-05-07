cd4367a48f71d3a03a00a19bd0e563759d51c3b2
Allen Antony <22706998@student.uwa.edu.au>
2021-05-08 00:28:53 +0800

    Added form for registration

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,9 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
+from app.models import User
+from app.models import Quiz
+
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
@@ -8,10 +11,23 @@ class LoginForm(FlaskForm):
     remember_me = BooleanField('Remember Me!')
     submit = SubmitField('Sign In')
 
+
 class RegisterForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
-    email = StringField('Email', validators=[DataRequired()])
-    preferred_name = StringField('Preferred Name', validators=[DataRequired()])
+    email = StringField('Email')
+    preferred_name = StringField('Preferred Name')
     password = StringField('Password', validators=[DataRequired()])
+    confirm_password = StringField('Confirm Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me!')
     submit = SubmitField('Register')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('That username is already taken')
+
+    def validate_email(self, email):
+        if email.data:
+            email = User.query.filter_by(email=email.data).first()
+            if email:
+                raise ValidationError('That email already has a registered account')
