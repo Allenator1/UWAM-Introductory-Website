3079d18147a01d6a7498c60faa0d71dfbec45957
Allen Antony <22706998@student.uwa.edu.au>
2021-05-13 16:58:55 +0800

    Added functions to get total number of correct answers for each section in the quiz

---
+++ app/controller.py
@@ -0,0 +1,26 @@
+from app.models import User, Quiz
+
+SECTION1_ANSWERS = {'Q' + i : 0 for i in range(1, 6)}
+SECTION2_ANSWERS = {'Q' + i : 0 for i in range(1, 6)} 
+SECTION3_ANSWERS = {'Q' + i : 0 for i in range(1, 6)}
+SECTION4_ANSWERS = {'Q' + i : 0 for i in range(1, 6)}
+SECTION5_ANSWERS = {'Q' + i : 0 for i in range(1, 6)}
+
+#--------------FUNCTIONS FOR ACCESSING QUIZ DATA------------------
+
+def get_totals(quiz):
+    totals = {}
+    totals['section1'] = get_total(quiz.section1, SECTION1_ANSWERS)
+    totals['section2'] = get_total(quiz.section2, SECTION2_ANSWERS)
+    totals['section3'] = get_total(quiz.section3, SECTION3_ANSWERS)
+    totals['section4'] = get_total(quiz.section4, SECTION4_ANSWERS)
+    totals['section5'] = get_total(quiz.section5, SECTION5_ANSWERS)
+    return totals
+
+def get_total(questions, answers):
+    total = 0
+    for key in questions.keys():
+        if answers[key] == questions[key]:
+            total += 1
+    return total
+
