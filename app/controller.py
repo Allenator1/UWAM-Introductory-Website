5c40cc39f1fb106a5c23493655862771e4a84b3f
Allen Antony <22706998@student.uwa.edu.au>
2021-05-13 22:13:43 +0800

    Used a list instead of a dictionary in get_section_totals()

--- app/controller.py
+++ app/controller.py
@@ -1,26 +1,28 @@
 from app.models import User, Quiz
+from app import db
 
-SECTION1_ANSWERS = {'Q' + i : 0 for i in range(1, 6)}
-SECTION2_ANSWERS = {'Q' + i : 0 for i in range(1, 6)} 
-SECTION3_ANSWERS = {'Q' + i : 0 for i in range(1, 6)}
-SECTION4_ANSWERS = {'Q' + i : 0 for i in range(1, 6)}
-SECTION5_ANSWERS = {'Q' + i : 0 for i in range(1, 6)}
+SECTION1_ANSWERS = {'Q' + str(i) : 0 for i in range(1, 6)}
+SECTION2_ANSWERS = {'Q' + str(i) : 0 for i in range(1, 6)} 
+SECTION3_ANSWERS = {'Q' + str(i) : 0 for i in range(1, 6)}
+SECTION4_ANSWERS = {'Q' + str(i) : 0 for i in range(1, 6)}
+SECTION5_ANSWERS = {'Q' + str(i) : 0 for i in range(1, 6)}
 
 #--------------FUNCTIONS FOR ACCESSING QUIZ DATA------------------
 
-def get_totals(quiz):
-    totals = {}
-    totals['section1'] = get_total(quiz.section1, SECTION1_ANSWERS)
-    totals['section2'] = get_total(quiz.section2, SECTION2_ANSWERS)
-    totals['section3'] = get_total(quiz.section3, SECTION3_ANSWERS)
-    totals['section4'] = get_total(quiz.section4, SECTION4_ANSWERS)
-    totals['section5'] = get_total(quiz.section5, SECTION5_ANSWERS)
+def get_section_totals(quiz):
+    totals = []
+    totals.append(get_total(quiz.section1, SECTION1_ANSWERS))
+    totals.append(get_total(quiz.section2, SECTION2_ANSWERS))
+    totals.append(get_total(quiz.section3, SECTION3_ANSWERS))
+    totals.append(get_total(quiz.section4, SECTION4_ANSWERS))
+    totals.append(get_total(quiz.section5, SECTION5_ANSWERS))
     return totals
 
 def get_total(questions, answers):
     total = 0
     for key in questions.keys():
-        if answers[key] == questions[key]:
+        if answers[key].lower() == questions[key].lower():
             total += 1
     return total
+
 
