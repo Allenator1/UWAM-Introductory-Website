ff86e678236c4e763476fb9476c6dac2ffc8febf
Allen Antony <22706998@student.uwa.edu.au>
2021-05-17 19:11:46 +0800

    Added answers to the answer dictionaries. Gave get_total function the ability to equate unordered lists.

--- app/controller.py
+++ app/controller.py
@@ -1,11 +1,11 @@
 from app.models import User, Quiz
 from app import db
 
-FINANCE_ANSWERS = {'Q' + str(i) : '' for i in range(1, 4)}
-MARKETING_ANSWERS = {'Q' + str(i) : '' for i in range(1, 4)} 
-VD_ANSWERS = {'Q' + str(i) : '' for i in range(1, 4)}
-CHASSIS_ANSWERS = {'Q' + str(i) : '' for i in range(1, 4)}
-POWERTRAIN_ANSWERS = {'Q' + str(i) : '' for i in range(1, 4)}
+FINANCE_ANSWERS = {'Q1':'c', 'Q2':'b', 'Q3':'a'}
+MARKETING_ANSWERS = {'Q1':'a', 'Q2':'d', 'Q3':'d'}
+VD_ANSWERS = {'Q1':'d', 'Q2':['a', 'b', 'c'], 'Q3':['c', 'd']}
+CHASSIS_ANSWERS = {'Q1':'b', 'Q2':'b', 'Q3':'a'}
+POWERTRAIN_ANSWERS = {'Q1':['a', 'c'], 'Q2':'d', 'Q3':'c'}
 
 #--------------FUNCTIONS FOR ACCESSING QUIZ DATA------------------
 
@@ -13,8 +13,8 @@ def get_section_totals(quiz):
     section_totals = {} 
     section_totals['Finance'] = get_total(quiz.finance, FINANCE_ANSWERS)
     section_totals['Marketing'] = get_total(quiz.marketing, MARKETING_ANSWERS)
-    section_totals['Chassis'] = get_total(quiz.chassis, VD_ANSWERS)
-    section_totals['Vehicle Dynamics'] = get_total(quiz.vehicle_dynamics, CHASSIS_ANSWERS)
+    section_totals['Chassis'] = get_total(quiz.chassis, CHASSIS_ANSWERS)
+    section_totals['Vehicle Dynamics'] = get_total(quiz.vehicle_dynamics, VD_ANSWERS)
     section_totals['Powertrain'] = get_total(quiz.powertrain, POWERTRAIN_ANSWERS)
     return section_totals
 
@@ -27,10 +27,12 @@ def get_proportions(totals):
     return percents
 
 
-def get_total(questions, answers):
+def get_total(section, answers):
     total = 0
-    for key in questions.keys():
-        if answers[key].lower() == questions[key].lower():
+    for key, val in section.items():
+        if type(val) == list and set(val) == set(answers[key]):
+            total += 1
+        elif type(val) == str and val == answers[key]:
             total += 1
     return total
 
